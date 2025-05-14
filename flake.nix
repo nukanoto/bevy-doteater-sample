1ad237a5ce8fc96a9003cda925ff0d6337406fc2
nukanoto <nukanoto@nukanoto.net>
2025-05-14 15:12:37 +0900

    nix: set up flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      rust-overlay,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (import inputs.rust-overlay)
+            ];
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              ];
+              packages = [ pkgs.cowsay ];
+            };
+          };
+        };
+    };
+}
